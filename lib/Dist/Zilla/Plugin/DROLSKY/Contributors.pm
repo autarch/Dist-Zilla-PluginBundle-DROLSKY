bdb6accd9383ac8e598576277c9a424706f194cc
Dave Rolsky <autarch@urth.org>
2015-07-12 12:43:00 -0500

    Bump version after release

--- lib/Dist/Zilla/Plugin/DROLSKY/Contributors.pm
+++ lib/Dist/Zilla/Plugin/DROLSKY/Contributors.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 
 use Moose;
 
--- lib/Dist/Zilla/Plugin/DROLSKY/License.pm
+++ lib/Dist/Zilla/Plugin/DROLSKY/License.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 
 use Module::Runtime qw( use_module );
 use String::RewritePrefix;
