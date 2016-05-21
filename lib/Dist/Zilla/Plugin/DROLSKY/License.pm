2e0328a5515e915a05bbd9fee28afb581490cab7
Dave Rolsky <autarch@urth.org>
2016-05-21 11:45:55 -0500

    Bump version after release

--- lib/Dist/Zilla/Plugin/DROLSKY/License.pm
+++ lib/Dist/Zilla/Plugin/DROLSKY/License.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '0.56';
+our $VERSION = '0.57';
 
 use Module::Runtime qw( use_module );
 use String::RewritePrefix;
--- lib/Pod/Weaver/PluginBundle/DROLSKY.pm
+++ lib/Pod/Weaver/PluginBundle/DROLSKY.pm
@@ -3,7 +3,7 @@ package Pod::Weaver::PluginBundle::DROLSKY;
 use strict;
 use warnings;
 
-our $VERSION = '0.56';
+our $VERSION = '0.57';
 
 use namespace::autoclean -also => ['_exp'];
 
