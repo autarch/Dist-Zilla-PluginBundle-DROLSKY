6bc6ac6f7df3c7df094bb87d766624047bc781f2
Dave Rolsky <autarch@urth.org>
2017-02-28 17:49:45 -0600

    Bump version after release

--- lib/Dist/Zilla/Plugin/DROLSKY/Git/CheckFor/CorrectBranch.pm
+++ lib/Dist/Zilla/Plugin/DROLSKY/Git/CheckFor/CorrectBranch.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '0.82';
+our $VERSION = '0.83';
 
 use Moose;
 
--- lib/Dist/Zilla/Plugin/DROLSKY/TidyAll.pm
+++ lib/Dist/Zilla/Plugin/DROLSKY/TidyAll.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '0.82';
+our $VERSION = '0.83';
 
 use Code::TidyAll::Config::INI::Reader 0.44;
 use List::Util 1.45 qw( uniqstr );
