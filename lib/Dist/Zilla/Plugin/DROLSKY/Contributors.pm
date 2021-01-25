2f103309e1f2ca3cf51a15bd6ee079318c89224f
Dave Rolsky <autarch@urth.org>
2021-01-24 18:02:45 -0600

    Bump version after release

--- lib/Dist/Zilla/Plugin/DROLSKY/Contributors.pm
+++ lib/Dist/Zilla/Plugin/DROLSKY/Contributors.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '1.18';
+our $VERSION = '1.19';
 
 use Moose;
 
--- lib/Dist/Zilla/PluginBundle/DROLSKY.pm
+++ lib/Dist/Zilla/PluginBundle/DROLSKY.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '1.18';
+our $VERSION = '1.19';
 
 use Devel::PPPort 3.42;
 use Dist::Zilla 6.0;
