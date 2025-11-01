e9ca658b47fc72899cb7fa257bacf2b19b22ab51
Dave Rolsky <autarch@urth.org>
2025-11-01 13:59:08 -0500

    Bump version after release

--- lib/Dist/Zilla/Plugin/DROLSKY/MakeMaker.pm
+++ lib/Dist/Zilla/Plugin/DROLSKY/MakeMaker.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '1.23';
+our $VERSION = '1.24';
 
 use File::Which qw( which );
 
--- lib/Dist/Zilla/PluginBundle/DROLSKY.pm
+++ lib/Dist/Zilla/PluginBundle/DROLSKY.pm
@@ -7,7 +7,7 @@ use warnings;
 use autodie;
 use namespace::autoclean;
 
-our $VERSION = '1.23';
+our $VERSION = '1.24';
 
 use Devel::PPPort 3.42;
 use Dist::Zilla 6.0;
